796e4ccbf6594354381cf4cb8653a817161d8e31
Lukasz Kielczewski <lukasz.kielczewski66@gmail.com>
2023-02-07 17:58:02 +0100

    add responsive navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexModule, FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { RoutingModule } from './routing/routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 const routes: Routes = [
@@ -27,11 +30,14 @@ const routes: Routes = [
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(routes),
+        FlexModule,
+        FlexLayoutModule,
         MatToolbarModule,
         MatIconModule,
         RoutingModule,
         MatSidenavModule,
-        MatListModule
+        MatListModule,
+        MatButtonModule,
     ]
 })
 export class AppModule { }
