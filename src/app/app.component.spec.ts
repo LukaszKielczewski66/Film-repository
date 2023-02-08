b8d2ba71352a4da8f51828c89642c8695ad8af70
Lukasz Kielczewski <lukasz.kielczewski66@gmail.com>
2023-02-08 21:29:41 +0100

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'films-repository'`, () => {
+  it(`should have as title 'movies-repository'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('films-repository');
+    expect(app.title).toEqual('movies-repository');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('films-repository app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('movies-repository app is running!');
   });
 });
